66026354e3387d377c1dcc1ee4340ad90c9e6533
Adolf <shestachok777@gmail.com>
2026-03-15 13:05:24 +0500

    init commit

---
+++ books/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    img_src = models.URLField(blank=True, null=True)
+    years_of_age = models.CharField(max_length=9)
+    bio = models.TextField()
+    works = models.TextField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'author'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+# Create your models here.
