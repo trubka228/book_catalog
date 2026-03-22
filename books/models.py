3354c9d50071bf0917daa50545eb03746db4eab6
Adolf <shestachok777@gmail.com>
2026-03-22 13:18:21 +0500

    created db

--- books/models.py
+++ books/models.py
@@ -14,6 +14,30 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    img_src = models.URLField(blank=True, null=True)
+    publication_date = models.DateField()
+    short_desc = models.TextField()
+
+    class Meta:
+        db_table = 'book'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+class BookDetail(models.Model):
+    book = models.OneToOneField(Book, on_delete=models.CASCADE,related_name='details')
+
+    description = models.TextField()
+
+    class Meta:
+        db_table = 'book_detail'
+
+    def __str__(self):
+        return f'Details of {self.book.title}'
 
 
 
